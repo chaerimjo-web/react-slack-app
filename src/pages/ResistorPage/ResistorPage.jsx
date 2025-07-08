68c0e9872097f7b454c2dd2c70a7b43861bdb311
chaerimjo-web <chaerim010710@gmail.com>
2025-07-08 10:23:04 +0900

    폼 유효성 검사 하기

--- src/pages/ResistorPage/ResistorPage.jsx
+++ src/pages/ResistorPage/ResistorPage.jsx
@@ -1,23 +1,74 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const ResistorPage = () => {
+  const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  //훅 폼에 제공되는 handleSubmit이 있음 -> onSubmit을 따로 만든다
+  const onSubmit = () => {};
+
   return (
     <div className="auth-wrapper">
       <div style={{ textAlign: "center" }}>
         <h3>ResistorPage</h3>
       </div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="email">Email </label>
-        <input type="email" type="email" id="email" />
+        <input
+          type="email"
+          type="email"
+          id="email"
+          {...register("email", {
+            required: true,
+            pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i,
+          })}
+          //유효성 통화하지 못하면 errors 실행 => errors true
+        />
+        {errors.email && <p>This email field is required </p>}
 
         <label htmlFor="name">Name </label>
-        <input type="name" type="name" id="name" />
+        <input
+          type="name"
+          type="name"
+          id="name"
+          {...register("name", {
+            required: true,
+            maxLength: 10,
+          })}
+        />
+        {errors.name && errors.name.type === "required" && (
+          <p>This name field is required </p>
+        )}
+        {errors.name && errors.name.type === "maxLength" && (
+          <p>Your input exceed maxinum length</p>
+        )}
 
         <label htmlFor="password">Password </label>
-        <input type="password" type="password" id="password" />
+        <input
+          type="password"
+          type="password"
+          id="password"
+          {...register("password", {
+            required: true,
+            minLength: 6,
+          })}
+        />
+        {errors.password && errors.password.type === "required" && (
+          <p>This password field is required </p>
+        )}
+        {errors.password && errors.password.type === "minLength" && (
+          <p>Password must have at least 6 characters</p>
+        )}
 
-				<input type="submit" disabled />
+        <input type="submit" disabled={loading} />
 
         <Link to={"/"} style={{ color: "gray", textDecoration: "none" }}>
           이미 아이디가 있다면 ...
