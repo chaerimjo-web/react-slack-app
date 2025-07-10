6aa027e84e73ad64a15bb92cfb5aab485d29956d
chaerimjo-web <chaerim010710@gmail.com>
2025-07-10 11:29:51 +0900

    로그인 인증된 이후의 페이지 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import ChatPage from "./pages/ChatPage/ChatPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import app from "./firebase";
+import { useEffect } from "react";
 
 function App() {
+  const auth = getAuth(app);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if(user) {
+        navigate('/');
+      } else {
+        navigate('/login');
+      }
+    })
+    return () => {
+      unsubscribe();
+    }
+  })
+
   return (
     <Routes>
       <Route path="/" element={<ChatPage />} />
